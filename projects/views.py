6d7093b51d1a0ddbe8247ee2071f2c4cf156ff6f
jamesstottmoj <james.stott@digital.justice.gov.uk>
2026-06-26 15:16:02 +0000

    comment out test code

--- projects/views.py
+++ projects/views.py
@@ -5,7 +5,6 @@ from django.views.generic.edit import DeleteView
 from django.views.generic.list import ListView
 
 from projects.models import Project, ProjectUserPermissions
-from users.models import User
 
 
 # Create your views here.
@@ -19,9 +18,9 @@ class ProjectListView(ListView):
         set PK to whatever user you've created in db.
         Code will be deleted once user auth is implemented
         """
-        pk = 4
-        user = User.objects.get(pk=pk)
-        return user.projects.all()
+        # pk = 4
+        # user = User.objects.get(pk=pk)
+        # return user.projects.all()
         # Once entra in - this line will be used to get the projects for the logged in user
         return self.request.user.projects.all()
 
