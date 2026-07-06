2caa6639c75528f4e3fee940f4638bae443c1a93
jamesstottmoj <james.stott@digital.justice.gov.uk>
2026-07-06 14:38:47 +0000

    formatting

--- js/components/add-another-autocomplete/add-another-autocomplete.js
+++ js/components/add-another-autocomplete/add-another-autocomplete.js
@@ -65,12 +65,16 @@ const AddAnotherAutocomplete = {
             .forEach((element) => element.remove());
 
           // Remove copied validation state from cloned rows.
-          newItem.querySelectorAll(".govuk-error-message").forEach((element) => {
-            element.remove();
-          });
-          newItem.querySelectorAll(".govuk-form-group--error").forEach((element) => {
-            element.classList.remove("govuk-form-group--error");
-          });
+          newItem
+            .querySelectorAll(".govuk-error-message")
+            .forEach((element) => {
+              element.remove();
+            });
+          newItem
+            .querySelectorAll(".govuk-form-group--error")
+            .forEach((element) => {
+              element.classList.remove("govuk-form-group--error");
+            });
 
           select.value = "";
           select.selectedIndex = 0;
