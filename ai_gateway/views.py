34bad1bebbe856bd69c489e711d945b46fc6a203
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2026-07-16 11:37:58 +0100

    Implement revoke key workflow (#94)

--- ai_gateway/views.py
+++ ai_gateway/views.py
@@ -3,10 +3,10 @@ from __future__ import annotations
 from functools import cached_property
 
 import sentry_sdk
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.utils.cache import add_never_cache_headers
-from django.views.generic import DetailView, FormView, ListView
+from django.views.generic import DeleteView, DetailView, FormView, ListView
 
 from ai_gateway.exceptions import AIGatewayError
 from ai_gateway.forms import KeyCreateForm
@@ -94,3 +94,25 @@ class KeyDetailView(ProjectScopedMixin, DetailView):
                 sentry_sdk.capture_exception(error)
                 context["models"] = []
         return context
+
+
+class KeyRevokeView(ProjectScopedMixin, DeleteView):
+    template_name = "ai_gateway/key-revoke.html"
+    context_object_name = "key"
+    http_method_names = ["get", "post", "head", "options"]
+
+    def get_queryset(self):
+        return self.project.ai_gateway_keys.all()
+
+    def get_success_url(self):
+        return self.project.get_absolute_keys_url()
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        with KeyService.from_settings() as service:
+            service.delete_key(self.object)
+        self.request.session["success_message"] = {
+            "heading": "Key revoked",
+            "message": f"You've revoked {self.object.name}",
+        }
+        return HttpResponseRedirect(self.get_success_url())
