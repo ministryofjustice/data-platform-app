e612dea93a95662f0c1540ec50d5437ecc94798c
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2026-07-21 07:46:50 +0000

    feat: add "Go to app" link for authenticated users in service navigation
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- tests/data_platform_app/test_views.py
+++ tests/data_platform_app/test_views.py
@@ -17,6 +17,20 @@ class TestHomeView:
         assert response.context["inverse_header"] is True
         assert "service_navigation_items" in response.context
 
+    def test_authenticated_user_sees_go_to_app_link(self, client, user):
+        client.force_login(user)
+
+        response = client.get(reverse("home"))
+
+        assert response.status_code == 200
+        assert b"Go to app" in response.content
+
+    def test_anonymous_user_does_not_see_go_to_app_link(self, client):
+        response = client.get(reverse("home"))
+
+        assert response.status_code == 200
+        assert b"Go to app" not in response.content
+
 
 class TestRoadmapView:
     """Tests for the RoadmapView at '/roadmap/'."""
@@ -33,6 +47,14 @@ class TestRoadmapView:
         assert response.context["inverse_header"] is True
         assert "service_navigation_items" in response.context
 
+    def test_authenticated_user_sees_go_to_app_link(self, client, user):
+        client.force_login(user)
+
+        response = client.get(reverse("roadmap"))
+
+        assert response.status_code == 200
+        assert b"Go to app" in response.content
+
 
 class TestDataFactoriesView:
     """Tests for the DataFactoriesView at '/data-factories/'."""
@@ -48,6 +70,14 @@ class TestDataFactoriesView:
         assert response.context["show_masthead"] is False
         assert response.context["inverse_header"] is True
         assert "service_navigation_items" in response.context
+
+    def test_authenticated_user_sees_go_to_app_link(self, client, user):
+        client.force_login(user)
+
+        response = client.get(reverse("data_factories"))
+
+        assert response.status_code == 200
+        assert b"Go to app" in response.content
 
 
 class TestLandingView:
@@ -66,6 +96,14 @@ class TestLandingView:
 
         assert response.status_code == 200
         assert "landing.html" in [t.name for t in response.templates]
+
+    def test_authenticated_user_does_not_see_go_to_app_link(self, client, user):
+        client.force_login(user)
+
+        response = client.get(reverse("landing"))
+
+        assert response.status_code == 200
+        assert b"Go to app" not in response.content
 
 
 class TestHealthcheckView:
