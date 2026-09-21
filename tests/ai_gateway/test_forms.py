faf4eaaf1992d0f9db72c5a6086b0c961a788107
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2026-09-21 09:52:16 +0000

    Remove unneeded test

--- tests/ai_gateway/test_forms.py
+++ tests/ai_gateway/test_forms.py
@@ -146,14 +146,6 @@ class TestModelUsageRateFormSet:
 
         assert not formset.is_valid()
 
-    def test_rejects_zero_submitted_forms(self):
-        formset = build_model_usage_rate_formset(
-            data=calculator_formset_management_data(total_forms=0),
-            available_models=AVAILABLE_MODELS,
-        )
-
-        assert not formset.is_valid()
-
     def test_formset_filters_model_choices_by_provider(self):
         formset = build_model_usage_rate_formset(
             data=calculator_formset_management_data()
