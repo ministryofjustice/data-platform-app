c581a2dacf676dd5357d4d08858ae9f47313c253
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2026-09-21 09:04:59 +0000

    Remove error message asserts

--- tests/ai_gateway/test_forms.py
+++ tests/ai_gateway/test_forms.py
@@ -145,7 +145,6 @@ class TestModelUsageRateFormSet:
         )
 
         assert not formset.is_valid()
-        assert "Please submit at least 1 form." in formset.non_form_errors()
 
     def test_rejects_zero_submitted_forms(self):
         formset = build_model_usage_rate_formset(
@@ -154,7 +153,6 @@ class TestModelUsageRateFormSet:
         )
 
         assert not formset.is_valid()
-        assert "Please submit at least 1 form." in formset.non_form_errors()
 
     def test_formset_filters_model_choices_by_provider(self):
         formset = build_model_usage_rate_formset(
